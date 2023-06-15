b0d79640314b25c4686da858e3f3b9c5bf97c2d0
rinikim <tytntu@naver.com>
2023-06-15 22:54:36 +0900

    feat : baseEntity 설정 (JpaAuditing 설정)
    - 생성일시, 수정일시를 entity에 row가 추가될 때 자동으로 추가하기 위해 JpaAuditing 설정
    - BaseEntity에 공통으로 묶음으로써 entity에 자유롭게 사용가능하도록 설정
    - 외부에서 수정되지 않도록 abstract class로 생성한 후 @MappedSuperclass 적용
    
    issue : #5

---
+++ src/main/java/com/dev/patientpractice/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.dev.patientpractice.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    protected LocalDateTime createdAt; // 생성일시
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    protected LocalDateTime updatedAt; // 수정일시
+}
