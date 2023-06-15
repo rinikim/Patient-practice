0605640c7bd1b3a99ba10abb20a98f076c591979
rinikim <tytntu@naver.com>
2023-06-15 21:27:09 +0900

    프로젝트 생성

---
+++ src/main/java/com/dev/patientpractice/PatientPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.dev.patientpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientPracticeApplication.class, args);
+    }
+
+}
