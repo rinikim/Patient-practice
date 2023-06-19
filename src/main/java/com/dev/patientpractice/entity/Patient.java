ece9e4ebae8f8bc148beb7ad4427074b1ab76c61
rinikim <tytntu@naver.com>
2023-06-19 16:23:45 +0900

    feat : 병원(Hospital), 환자(Patient), 환자방문(Visit) entity 생성
    - 병원과 환자는 양방향 관계를 맺고 있다.
    - 환자와 환자방문은 양방향 관계를 맺고 있다.
    - 병원과 환자방문은 다대일 단방향만 맺고 있다.
    - 병원이 삭제될 경우 환자이력이 남을 수 있도록 설정
    - 환자가 삭제될 경우 환자방문이력이 함께 삭제될 수 있도록 설정
    
    issue : #5

---
+++ src/main/java/com/dev/patientpractice/entity/Patient.java
@@ -0,0 +1,42 @@
+package com.dev.patientpractice.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Patient extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hospital_id", nullable = false)
+    private Hospital hospital;  // 병원
+
+    @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Visit> visits = new ArrayList<>();  // 환자방문
+
+    @Column(nullable = false, length = 45)
+    private String name;  // 환자명
+
+    @Column(nullable = false, length = 13)
+    private String registrationNumber;  // 환자등록번호
+
+    @Column(nullable = false, length = 10)
+    private String genderCode;  // 성별코드
+
+    @Column(length = 10)
+    private String birthDate;  // 생년월일
+
+    @Column(length = 20)
+    private String phoneNumber;  // 휴대전화번호
+
+}
