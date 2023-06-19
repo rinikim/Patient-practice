6e37c0883dc6becbb25b8389226f783dfcc12bbf
rinikim <tytntu@naver.com>
2023-06-19 16:38:55 +0900

    feat : 코드그룹(CodeGroup), 코드(Code) 엔티티 생성
    - 코드그룹과 코드는 양방향으로 관계를 맺고 있다.
    
    issue : #5

---
+++ src/main/java/com/dev/patientpractice/entity/CodeGroup.java
@@ -0,0 +1,28 @@
+package com.dev.patientpractice.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class CodeGroup extends BaseEntity {
+
+    @Id
+    @Column(name = "code_group", nullable = false, length = 10)
+    private String codeGroup;  // 코드그룹
+
+    @OneToMany(mappedBy = "codeGroup", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Code> codes = new ArrayList<>();  // 코드
+
+    @Column(nullable = false, length = 10)
+    private String name;  // 코드그룹명
+
+    @Column(nullable = false, length = 10)
+    private String description;  // 설명
+}
