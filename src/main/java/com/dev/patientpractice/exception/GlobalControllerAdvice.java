8560e5806c5bf088bff8b4b534339f8e3eddefe9
rinikim <tytntu@naver.com>
2023-06-20 20:35:38 +0900

    feat : 파라미터 유효성 관련 exception handler 추가
    - MethodArgumentNotValidException : 잘못 된 request data 를 입력했을 때 발생하는 exception (validation 을 사용했을 경우 message 를 반환해줌)
    - HttpMessageNotReadableException : request data 필드 중 날짜데이터를 잘못 넣었을 때 발생하는 exception
    
    issue : #7

--- src/main/java/com/dev/patientpractice/exception/GlobalControllerAdvice.java
+++ src/main/java/com/dev/patientpractice/exception/GlobalControllerAdvice.java
@@ -3,6 +3,8 @@ package com.dev.patientpractice.exception;
 import com.dev.patientpractice.dto.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +17,18 @@ public class GlobalControllerAdvice {
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
                 .body(Response.error(e.getErrorCode().name(), e.getMessage()));
 
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    private ResponseEntity<?> handleMethodArgumentValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(ErrorCode.INVALID_PARAMETER.getHttpStatus())
+                .body(Response.error(ErrorCode.INVALID_PARAMETER.name(), e.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    private ResponseEntity<?> handleMethodArgumentValidException(HttpMessageNotReadableException e) {
+        return ResponseEntity.status(ErrorCode.INVALID_PARAMETER.getHttpStatus())
+                .body(Response.error(ErrorCode.INVALID_PARAMETER.name(), e.getCause().getCause().getLocalizedMessage()));
     }
 
     @ExceptionHandler(RuntimeException.class)
