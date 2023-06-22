5bd80170c82973a4c2c24e032e2781a988505247
rinikim <tytntu@naver.com>
2023-06-22 09:33:20 +0900

    fix : 환자방문 수정 ,삭제 시 방문상태코드 조건 추가
    - 환자방문 수정, 삭제 시 방문상태코드는 방문중(1), 종료(2) 인 부분만 조회한다. (삭제되지 않은 부분만 조회)
    
    issue : #33

--- src/main/java/com/dev/patientpractice/repository/VisitRepository.java
+++ src/main/java/com/dev/patientpractice/repository/VisitRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface VisitRepository extends JpaRepository<Visit, Long> {
 
     Page<Visit> findAllByPatient_Id(Long patientId, Pageable pageable);
+    Optional<Visit> findByIdAndVisitStatusCodeIn(Long visitId, List<String> visitStatusCodes);
 }
